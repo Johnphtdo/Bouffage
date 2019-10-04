01388a271b3bf2fe6f17d35f4cad0da6e0cea55f
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-10-04 10:17:39 -0500

    change variables

--- assets/javascript/drink.js
+++ assets/javascript/drink.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-    var drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?";
-    var drinkChoicesUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic";
-    var liquorType = "Gin";
+    var drinkUrl = "https://the-cocktail-db.p.rapidapi.com/list.php?i=list";
+    var drinkAPI = "f583c76719msh4620570ef703476p1f33bdjsn9768c87b23c1";
+    var drinkChoices = "";
+    var liquorType = "";
 
 
-    var drinkQueryUrl = drinkUrl + "a=" + drinkChoicesUrl + "i=" + liquorType;
+    var drinkQueryUrl = drinkUrl+ "apikey=" + drinkAPI + "a=" + drinkChoices + "i=" + liquorType;
 
 
     $.ajax({
         url: drinkQueryUrl,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        console.log(drinkUrl);
-        console.log(drinkChoicesUrl)
+       
 
     });
+
+    $("#drinkcoicebtn").on("click", function(event) {
+
+
 })
+
+
