e3ac37a6bc75737a40a3904e833c32d82631a3dc
Adam Norton <adam@adamnorton.com>
2019-10-03 19:18:34 -0500

    fixed background images to stay full screen and fixed... no scroll. Added icons to buttons. Put in random food quotes at the bottom of the page. Resized some things to make everything fit better.  The logo text is a blurry image... needs to be higher resolution. Add Bootstrap rrows and columns to make the layout more stable and will help later with responsive-ness. Added img-fluid class to logo to make is resize with window.

--- assets/javascript/visual.js
+++ assets/javascript/visual.js
@@ -1,11 +1,12 @@
 var backArr = ["url(./assets/images/food_back01.jpg)", "url(./assets/images/food_back03.jpg)", "url(./assets/images/food_back04.jpg)", "url(./assets/images/food_back05.jpg)", "url(./assets/images/food_back06.jpg)", "url(./assets/images/food_back07.jpg)", "url(./assets/images/food_back08.jpg)", "url(./assets/images/food_back09.jpg)", "url(./assets/images/food_back10.jpg)", "url(./assets/images/food_back11.jpg)"];
 
-var quoteArr = ["Food is an important part of a balanced diet!", "I cook with wine. Sometimes I even add it to the food.", "Cooling is like love. It should be intered into with abandon, or not at all.", "Life is a combination of magic and pasta.", "Promises and pie-crust are made to be broken", "Cheese - milk's leap towards immortality.", "Rice is great if you're hungry and want 2,000 of something.", "It would be a sad waste of opportuity to eat badly.", "First we eat. Then we do everything else.", "A balanced diet is a cookie in each hand.", "Ask not what you can do for your country. Ask what's for lunch." ];
+var quoteArr = ["Food is an important part of a balanced diet!", "I cook with wine. Sometimes I even add it to the food.", "Cooking is like love. It should be entered into with abandon, or not at all.", "Life is a combination of magic and pasta.", "Promises and pie-crust are made to be broken", "Cheese - milk's leap towards immortality.", "Rice is great if you're hungry and want 2,000 of something.", "It would be a sad waste of opportuity to eat badly.", "First we eat. Then we do everything else.", "A balanced diet is a cookie in each hand.", "Ask not what you can do for your country. Ask what's for lunch." ];
 
 var randomBackgroundNumber = Math.floor(Math.random() * backArr.length);
 var randomQuoteNumber = Math.floor(Math.random() * backArr.length);
 
 document.getElementById('background').style.backgroundImage = backArr[randomBackgroundNumber];
 // document.getElementById('qoute').innerHTML = quoteArr[randomQuoteNumber];
+$("#quote").text('"' + quoteArr[randomQuoteNumber] + '"');
 
 
