15a319d0a5e29fb9ea3f73e0c92c9ac4944bfcad
Dohn Joe <johnphtdo@gmail.com>
2019-10-04 11:16:09 -0500

    adding variables for get response and testing output of variables into the food.html

--- assets/javascript/food.js
+++ assets/javascript/food.js
@@ -3,12 +3,12 @@ var foodUrl = "https://api.edamam.com/search";
 var foodAPI = "&app_id=$b64e001c&app_key=$8203f05bcefbb343beaae6139be4661d";
 var foodItem = "chicken"
 var foodTimeFrame = "lunch"
-var foodCal = 1500;
-var foodDiet = "vegetarian"
+var foodCal = "1000-2000";
+var foodDiet = "alcohol-free"
 var foodAllergies = "peanuts"
 
 var foodQueryUrl = foodUrl +"?q="+ foodItem +  "&mealType=" +  foodTimeFrame +  "&calories=" +  foodCal +  "&health=" +  foodDiet +  "&excluded=" +  foodAllergies + foodAPI;
-
+console.log(foodQueryUrl)
 
   $.ajax({
     url: foodQueryUrl,
@@ -16,7 +16,17 @@ var foodQueryUrl = foodUrl +"?q="+ foodItem +  "&mealType=" +  foodTimeFrame +  
   }).then(function(response) {
     var foodInfo = response.hits
     console.log(foodInfo)
-    
+
+    var foodName = foodInfo[0].recipe.label
+    var foodImg = foodInfo[0].recipe.image
+    var recipeUrl = foodInfo[0].recipe.url
+    var yield = foodInfo[0].recipe.yield
+    var calCount = foodInfo[0].recipe.calories
+
+    $(`.food-name`).text(foodName)
+    $(`.foodImg`).attr("src",foodImg)
+    $(`.food-info`).html(recipeUrl)
+    console.log(foodImg)
   })
 
 
