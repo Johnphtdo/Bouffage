c777a08f926ca19693f4403a442907d7361c4030
Dohn Joe <johnphtdo@gmail.com>
2019-10-05 15:48:53 -0500

    added some ID's on the food.html to avoid conflicts with drink.html. Added css display for the food page to be hidden until on click. Form is functional and working with search options

--- assets/javascript/food.js
+++ assets/javascript/food.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
+$(`#foodBtn`).on("click",function(){
+ // Changing the css of #foodCol to reveal after information has been pushed
+   $(`#foodCol`).css("display","block")
+
 var foodUrl = "https://api.edamam.com/search";
 var foodAPI = "&app_id=$b64e001c&app_key=$8203f05bcefbb343beaae6139be4661d";
-var foodItem = "chicken"
-var foodTimeFrame = "lunch"
-var foodCal = "1000-2000";
-var foodDiet = "alcohol-free"
-var foodAllergies = "peanuts"
+var foodItem = $(`#mainIngredient`).val().trim();
+var foodTimeFrame = $(`#mealTypes`).val().trim();
+var foodCal = `500-${$(`#customRange2`).val().trim()}`;
+var foodDiet = $(`#dietTypes`).val().trim()
+var foodHealth = "";
+  if ($(`#healthTypes` == "none")){
 
-var foodQueryUrl = foodUrl +"?q="+ foodItem +  "&mealType=" +  foodTimeFrame +  "&calories=" +  foodCal +  "&health=" +  foodDiet +  "&excluded=" +  foodAllergies + foodAPI;
+    foodHealth = "alcohol-free"
+
+  }
+  else{
+
+    foodHealth = $(`#healthTypes`).val().trim();
+
+  }
+
+var foodAllergies = $(`#allergenTypes`).val().trim();
+
+var foodQueryUrl = foodUrl +"?q="+ foodItem +  "&mealType=" +  foodTimeFrame +  "&calories=" +  foodCal + "&diet=" + foodDiet + "&health=" +  foodHealth +  "&excluded=" +  foodAllergies + foodAPI;
 console.log(foodQueryUrl)
 
   $.ajax({
@@ -20,7 +36,7 @@ console.log(foodQueryUrl)
     var foodInfo = response.hits
     var foodName = foodInfo[i].recipe.label
     var foodImg = foodInfo[i].recipe.image
-    var recipeUrl = $(`<a href="">Recipe Link</a>`)
+    var recipeUrl = $(`<br> <a href="" target="_blank">Recipe Link</a>`)
     recipeUrl.attr("href",foodInfo[i].recipe.url)
     var foodYield = foodInfo[i].recipe.yield
     var calCount = Math.round(foodInfo[i].recipe.calories)
@@ -37,15 +53,15 @@ console.log(foodQueryUrl)
       $(`#food-info${[i]}`).append(itemList)
     }  
     $(`#food-info${[i]}`).append(recipeUrl)
-    
+   
     // Testing and debugging
-  //   console.log(foodInfo)
-  //   console.log(ingredients)
-  //   console.log(foodImg)
+    // console.log(foodInfo)
+    // console.log(ingredients)
+    // console.log(foodImg)
   }
   })
 
-
+})
 })
 
 
