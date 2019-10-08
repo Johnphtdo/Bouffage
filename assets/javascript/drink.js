da1990805d2bdd7737c688a6f64cae2ffa80607a
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-10-08 14:34:39 -0500

    made some changes to make the js FINALLY push to the app

--- assets/javascript/drink.js
+++ assets/javascript/drink.js
@@ -19,6 +19,8 @@ $(document).ready(function () {
                 "x-rapidapi-key": "f583c76719msh4620570ef703476p1f33bdjsn9768c87b23c1"
             }
         }
+
+        
         console.log(settings)
         $.ajax(settings).then(function (response) {
             console.log(response);
@@ -29,30 +31,84 @@ $(document).ready(function () {
             var drinkName = drinkInfo[i].strDrink;
             var drinkImage = drinkInfo[i].strDrinkThumb;
             var drinkId = drinkInfo[i].idDrink;
-            var instructions = drinkInfo[i].strInstructions;
-            var ingredients = drinkInfo[i].strIngredient;
-            var measurments = drinkInfo[i].strMeasure;
-        
+            
             // Added see more button to all the cards 
-            var addBtn = $(`<br><button type="button" id="seemore${[i]}">See More</button>`)
+            // var addBtn = $(`<button type="button" id="seemore-btn${[i]}">See More</button>`)
             
             //Pushing the information from the API into the HTML 
-            var mainDiv = $("<div class='col-sm-3 card m-1 meal drink'>");
-            var img = $("<img class='img-fluid drinkImg'>").attr({src: drinkImage, alt: drinkName});
-            var cardDiv = $("<div class='card-img-overlay'>");
-            var nameDiv = $("<h5 class='card-title text-white drinkName'>").text(drinkName).appendTo(cardDiv);   
-            var seeMore = $(drinkId ,instructions, ingredients ,measurments);
+
+            var cardDiv = $(` <div class="col-sm-3 card m-1 meal" id="food-card0" >
+            <img class="img-fluid foodImg" id="foodImg0" src="${drinkImage}" class="card-img-top" alt="${drinkName}">
+            <div class="card-img-overlay">
+                <h5 class="card-title text-white food-name" id="food-name0">${drinkName}</h5>
+            </div>
+            <div class="card-body">
+                <p class="card-text food-info" id="food-info0">
+                
+                </p>
+            </div>
+            </div>`);
+
+        //     var mainDiv = $("<div class='col-sm-3 card m-1 meal drink'>");
+        //     var img = $("<img class='img-fluid drinkImg card-img-top'>").attr({src: drinkImage, alt: drinkName});
+        //     var cardDiv = $("<div class='card-img-overlay'>");
+        //     var nameDiv = $("<h5 class='card-title text-white drinkName'>").text(drinkName).appendTo(cardDiv);   
+            var idSettings = {
+
+                "async": true,
+                "card": cardDiv,
+                "crossDomain": true,
+                "url": "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=" + drinkId,
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+                    "x-rapidapi-key": "f583c76719msh4620570ef703476p1f33bdjsn9768c87b23c1"
+        
+                }
+            }
+
+            $.ajax(idSettings).then(function(idResponse){
+                console.log(idResponse)
+                console.log(this.card)
+                
+
+                var ingredients = [];
+                for (let j=1; j < 15; j++){
+                  if (idResponse.drinks[0][`strIngredient${j}`] === null){
+                        break
+                    }
+                    else{
+                        var currentIngredient=idResponse.drinks[0][`strIngredient${j}`];
+                        ingredients.push(currentIngredient)
+                   
+
+                        $(this.card.find("#food-info0")).append($("<p>"+currentIngredient+"</p>"))
+                        
+                    }
+
+                }
             
-            $(`#drinkInfo${[i]}`).append(addBtn);
+               
+            
+                console.log(ingredients)
+                
+                
+            })
+            
+            
+            // mainDiv.append(nameDiv);
+            // mainDiv.append(cardDiv);
+            // mainDiv.append(img);
+            // mainDiv.append(addBtn);
            
-           
-           mainDiv.append(img);
-           mainDiv.append(nameDiv);
-           mainDiv.append(cardDiv);
-           mainDiv.append(addBtn)
-           mainDiv.append(seeMore);
-         
-           $("#drinksDiv").append(mainDiv);
+            
+            
+            
+            
+            $("#drinksDiv").append(cardDiv);
+            // $(`#drinkInfo${[i]}`).append(addBtn);
+          
+          
            
 
         }
@@ -63,6 +119,9 @@ $(document).ready(function () {
 
 
 
+        // $("#seemore-btn").on("click", function (event){
+
+        // })
     })
 });
 
