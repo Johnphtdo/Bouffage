25f1836d7ab1ec546643b5361f04fb0867919fe9
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-10-08 10:00:45 -0500

    small fixes

--- assets/javascript/drink.js
+++ assets/javascript/drink.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
 
-    $("")
+    
     $("#generate-btn").on("click", function (event) {
         var drinkAPI = "f583c76719msh4620570ef703476p1f33bdjsn9768c87b23c1";
         var drinkUrl = "https://www.thecocktaildb.com/api/json/v1/" + drinkAPI + "/filter.php?" + "/lookup.php?"
@@ -25,25 +25,25 @@ $(document).ready(function () {
             //For loop to push information to i
             for (let i = 0; i < 3; i++){
             //Setting variables for the information that we want to use in the respnse 
-            var drinkInfo = response.drinks
-            var drinkName = drinkInfo[i].strDrink
-            var drinkImage = drinkInfo[i].strDrinkThumb
-            var drinkId = drinkInfo[i].idDrink
-            var drinkInstructions = drinkInfo[i].strInstructions
-            var drinkIngredients = drinkInfo[i].strIngredient
-            var drinkMeasurments = drinkInfo[i].strMeasure
+            var drinkInfo = response.drinks;
+            var drinkName = drinkInfo[i].strDrink;
+            var drinkImage = drinkInfo[i].strDrinkThumb;
+            var drinkId = drinkInfo[i].idDrink;
+            var instructions = drinkInfo[i].strInstructions;
+            var ingredients = drinkInfo[i].strIngredient;
+            var measurments = drinkInfo[i].strMeasure;
         
             // Added see more button to all the cards 
-            var addBtn = $(`<br><button type="button" id="addBtn${[i]}">See More</button>`)
+            var addBtn = $(`<br><button type="button" id="seemore${[i]}">See More</button>`)
             
             //Pushing the information from the API into the HTML 
             var mainDiv = $("<div class='col-sm-3 card m-1 meal drink'>");
-            var img = $("<img class='img-fluid drinkImg'>").attr({src: drinkImage, alt: drinkName})
+            var img = $("<img class='img-fluid drinkImg'>").attr({src: drinkImage, alt: drinkName});
             var cardDiv = $("<div class='card-img-overlay'>");
-            var nameDiv = $("<h5 class='card-title text-white drinkName'>").text(drinkName).appendTo(cardDiv)     
-            var seeMore = $(drinkId ,drinkInstructions, drinkIngredients ,drinkMeasurments)
+            var nameDiv = $("<h5 class='card-title text-white drinkName'>").text(drinkName).appendTo(cardDiv);   
+            var seeMore = $(drinkId ,instructions, ingredients ,measurments);
             
-            
+            $(`#drinkInfo${[i]}`).append(addBtn);
            
            
            mainDiv.append(img);
@@ -56,9 +56,7 @@ $(document).ready(function () {
            
 
         }
-    //   var cardInfo0 = $(`#drink-card0`);
-    //   var cardInfo1 = $(`#drink-card1`);
-    //   var cardInfo2 = $(`#drink-card2`);
+   
   
         });
 
